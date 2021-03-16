12a52fc8e8951df85d2adbd84500fd46a1c2556f
gpmao <673720904@qq.com>
2021-03-16 23:37:29 +0800

    :art:refact:补充babel插件优化打包

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,15 @@
 
 const plugins = [
-  ["import", { "libraryName": "ant-design-vue", "libraryDirectory": "es", "style": "css" }] // `style: true` 会加载 less 文件
+  [
+    "import", 
+    { 
+      "libraryName": "ant-design-vue",
+      "libraryDirectory": "es",
+      "style": "css" 
+    }
+  ], // `style: true` 会加载 less 文件
+  "dynamic-import-node",
+  "@babel/plugin-transform-runtime"
 ]
 if (process.env.NODE_ENV === 'production') {
   plugins.push(["transform-remove-console", { "exclude": [ "error", "warn", "debug"] }]);
